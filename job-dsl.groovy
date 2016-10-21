5103db9edf4983135d625005ea6a46b0c15f620c
pbuditi <pbuditi@gmail.com>
2016-10-22 04:12:00 +0530

    feature env

--- job-dsl.groovy
+++ job-dsl.groovy
@@ -2,7 +2,10 @@ def views_json = readFileFromWorkspace("views.json")
 def views = new groovy.json.JsonSlurper().parseText(views_json)["views"]
 
 def tasks_json = readFileFromWorkspace("tasks.json")
-def tasks = new groovy.json.JsonSlurper().parseText(views_json)["tasks"]
+def tasks = new groovy.json.JsonSlurper().parseText(tasks_json)["tasks"]
+
+def env_json = readFileFromWorkspace("DEV.json")
+def env = new groovy.json.JsonSlurper().parseText(env_json)
 
 
 def gitCred = "c79906d0-32e2-41bd-bcef-5d447125a3eb"
@@ -23,6 +26,7 @@ views.each { view ->
 			}
 			steps {
 				shell("echo this is job ${task}")
+				shell("echo this is job ${env.name}")
 			}
 		}
 	}
